2396b72831769eee33bc95fee7a2e34e0be5050a
ryancheng44 <ryancheng44@gmail.com>
2024-03-17 08:11:36 -0400

    Moved all files into repository

---
+++ AngryBirdsReplica/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject deathEffect;
+
+    public float health = 4f;
+
+    public static int EnemiesAlive = 0;
+
+    void Start()
+    {
+        EnemiesAlive++;
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        if (collisionInfo.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0)
+            Debug.Log("LEVEL WON!");
+
+        Destroy(gameObject);
+    }
+}
